c7cd6b05cdeac706cea7672b5ccaeab0572724ee
yunjikwak <mia007478@gmail.com>
2023-11-18 23:10:02 +0900

    router 추가

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,24 @@
 import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Kakao from "./Kakao";
 import { Fragment } from "react";
 import { Moonhaeryuk } from "./pages/Moonhaeryuk";
-
-/* test */
+import { ResultPage } from "./pages/ResultPage";
 
 function App() {
+  const NotFound = () => {
+    return <h1>잘못된 주소 접근</h1>;
+  };
+
   return (
-    <div>
-      <Fragment>
-        <Moonhaeryuk />
-      </Fragment>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Moonhaeryuk />} />
+        <Route path="/result" element={<ResultPage />} />
+
+        <Route path="/*" element={<NotFound />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ client/src/pages/ResultPage.jsx
@@ -0,0 +1,9 @@
+import { Fragment } from "react";
+
+export const ResultPage = () => {
+  return (
+    <Fragment>
+      <div>결과 페이지</div>
+    </Fragment>
+  );
+};
