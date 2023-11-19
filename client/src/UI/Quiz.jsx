4131d6918195eff122bbb2c93a680b6589e38652
sujeong <qkrtnwjd4212@naver.com>
2023-11-20 06:48:05 +0900

    문제 해설 추가

--- client/src/UI/Quiz.jsx
+++ client/src/UI/Quiz.jsx
@@ -3,7 +3,9 @@ const Quiz = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
+  text-align: center;
   width: 85vw;
+  margin: 10px 0 20px 0;
   @media (min-width: 800px) {
     width: 320px;
   }
--- client/src/UI/Solution.jsx
+++ client/src/UI/Solution.jsx
@@ -3,10 +3,13 @@ const Solution = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
+  //text-align: center;
   margin-top: 2vh;
+  margin-bottom: 20px;
   width: 95vw;
   @media (min-width: 800px) {
     width: 320px;
   }
+  line-height: 1.5;
 `;
 export default Solution;
